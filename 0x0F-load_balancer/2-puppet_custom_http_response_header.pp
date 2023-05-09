1ea52cb1dd9abae3912235b6d89c2b73456d23b7
OzimiWhoke <ozimi11zz@gmail.com>
2023-05-09 00:59:38 -0700

    webservers

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-#  creating a custom HTTP header response, but with Puppet.
+# creating a custom HTTP header response, but with Puppet.
 # The name of the custom HTTP header must be X-Served-By
 # The value of the custom HTTP header must be the hostname of the server Nginx is running on
 
