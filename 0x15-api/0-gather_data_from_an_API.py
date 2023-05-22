f4ec994e826cf55c70039996958d021addb6c4d8
OzimiWhoke <ozimi11zz@gmail.com>
2023-05-22 06:40:05 -0700

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
+"""using this REST API, for a given employee ID, returns information"""
 
 import requests
+import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-
-    if "name" not in employee_data:
-        print("Employee not found.")
-        return
-
-    employee_name = employee_data["name"]
-
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    completed_tasks = [task for task in todos_data if task["completed"]]
-    number_of_completed_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos_data)
-
-    print(f"Employee {employee_name} is done with tasks ({number_of_completed_tasks}/{total_number_of_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
-
-employee_id = 1
-get_employee_todo_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
