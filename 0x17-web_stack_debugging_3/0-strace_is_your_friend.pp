2e743ba6d9bec0f4e2abf59e7d1453f6da60f562
OzimiWhoke <ozimi11zz@gmail.com>
2023-06-08 13:04:23 -0700

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
 # Using strace, find out why Apache is returning a 500 error using puppet
 
-exec { 'fix-apache-error':
-  command => '/path/to/fix-command',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
